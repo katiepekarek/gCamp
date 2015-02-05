5b323937d21790bce34fefbaba93e6168982a95c
katie Pekarek <katie_pekarek@yahoo.com>
2015-02-05 16:36:30 -0700

    faq controller and common question view page

---
+++ app/models/common_question.rb
@@ -0,0 +1,8 @@
+class CommonQuestion
+  attr_reader :question, :answer, :slug
+  def initialize(question, answer, slug)
+    @question = question
+    @answer = answer
+    @slug = slug
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  get 'terms', to:'terms#index'
-  get 'about', to:'about#index'
-  get 'faq', to:'faq#index'
+  get '/terms', to:'terms#index'
+  get '/about', to:'about#index'
+  get '/faq', to:'common_questions#index'
 
 end
   # The priority is based upon order of creation: first created -> highest priority.
