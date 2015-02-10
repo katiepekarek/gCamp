18636e1348501231afe9f614f5bc40ad666f1d37
katie Pekarek <katie_pekarek@yahoo.com>
2015-02-10 13:58:58 -0700

    add green alert messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
 
     if @task.save
-      flash[:notice] = "Task was successfully created."
+      flash[:success] = "Task was successfully created."
 
       respond_with(@task) do |format|
       format.html { render }
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      flash[:notice] = "Task was successfully updated."
+      flash[:success] = "Task was successfully updated."
 
       redirect_to tasks_path
 
