0deaecaf5e0fb85adaecd4d43d6caa35351352b1
katie Pekarek <katie_pekarek@yahoo.com>
2015-02-11 14:30:30 -0700

    centered flash message text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209015913) do
+ActiveRecord::Schema.define(version: 20150211211347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150209015913) do
     t.string   "text"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "completed"
   end
 
 end
