3abce4b169609b3de67eaeecb0fe18d0b1ff28ec
katie Pekarek <katie_pekarek@yahoo.com>
2015-03-02 14:34:28 -0700

    add sign-up form for users includes navbar links to sign up sign in and sign out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,7 @@ class RegistrationsController < ApplicationController
     @user = User.new(params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
+      flash[:sign_up] = "You have successfully signed up"
       redirect_to root_path
     else
       render :new
