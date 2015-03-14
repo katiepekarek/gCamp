a9ab58c396a4fe5cfc1f1989a8bfe7142422c990
katie Pekarek <katie_pekarek@yahoo.com>
2015-03-13 22:23:39 -0600

    add a dependent delete method and messages to projects show page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
 
-  has_many :tasks#, dependent: :destroy
+  has_many :tasks, dependent: :destroy
   has_many :memberships
   has_many :users, :through => :memberships
 end
